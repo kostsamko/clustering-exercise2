466365a98eae7f191fb756e77001543668a61d08
TSAMKOSOGLOU Konstantinos <P70556@nrb.be>
2021-12-29 23:06:23 +0200

    fix in removing features, start quantization

--- exercise2.m
+++ exercise2.m
@@ -5,7 +5,8 @@ features_names = {'Child mortality','Exports','Health','Imports','Income','Infla
 min_features_values = min(Countrydata);
 max_features_values = max(Countrydata);
 figure(1), hold on
-for i=1:9
+[~,length] = size(features_names);
+for i=1:length
     subplot(3,3,i)
     histogram(Countrydata(:,i));
     title(features_names{i}) 
@@ -19,70 +20,76 @@ std_features_values = std(Countrydata);
 coefficient_matrix = corrcoef(Countrydata);
 
 %feature selection
-%based on observations in the coefficient_matrix_ we can remove feature 1
-%(Child_mortality) and 5 (Income ) because they are highly correlated with 8 (Total_fertality) and 9 (GDPP)
-Countrydata(:,1) = [];
-Countrydata(:,5) = [];
-
+%based on observations in the coefficient_matrix_ we can remove feature 8
+%(Total_fertality) and 9(GDPP) because they are highly correlated with 1 (Child_mortality) and 5 (Income)
+Countrydata(:,8) = [];
+Countrydata(:,8) = [];
+features_names = setdiff(features_names, features_names{8});
+features_names = setdiff(features_names, features_names{8});
 % normalize the data 
 
 % standard score normalization
 standard_score = zscore(Countrydata);
+coefficient_standard_score = corrcoef(Countrydata);
 
 % min max normalization
 min_max_normalization = normalize(Countrydata, 'range');
+coefficient_min_max_normalization = corrcoef(Countrydata);
+
+% We don't remove other features
 
 %CLUSTERING METHODS
+number_of_clusters = 10;
 
 % k-means
-[best_thetas_k_means_zscore,best_bel_k_means_zscore,best_J_k_means_zscore] = k_algorithms(standard_score',10, 1000, 'k_means');
+[best_thetas_k_means_zscore,best_bel_k_means_zscore,best_J_k_means_zscore] = k_algorithms(standard_score',number_of_clusters, 1000, 'k_means');
 % plot elbow curve to find the number of clusters
-figure(2), plot(2:10,best_J_k_means_zscore(2:end))
+figure(2), plot(2:number_of_clusters,best_J_k_means_zscore(2:end))
 title('K-means elbow plot - zscore normalization')
 hold off
 
-[best_thetas_k_means_min_max,best_bel_k_means_min_max,best_J_k_means_min_max] = k_algorithms(min_max_normalization',10, 1000, 'k_means');
+[best_thetas_k_means_min_max,best_bel_k_means_min_max,best_J_k_means_min_max] = k_algorithms(min_max_normalization',number_of_clusters,1000, 'k_means');
 % plot elbow curve to find the number of clusters
-figure(3), plot(2:10,best_J_k_means_min_max(2:end))
+figure(3), plot(2:number_of_clusters,best_J_k_means_min_max(2:end))
 title('K-means elbow plot - min max normalization')
 hold off
 
 % k-medians
-[best_thetas_k_medians_zscore,best_bel_k_medians_zscore,best_J_k_medians_zscore] = k_algorithms(standard_score',10, 1000, 'k_medians');
+[best_thetas_k_medians_zscore,best_bel_k_medians_zscore,best_J_k_medians_zscore] = k_algorithms(standard_score',number_of_clusters, 1000, 'k_medians');
 % plot elbow curve to find the number of clusters
-figure(4), plot(2:10,best_J_k_medians_zscore(2:end))
+figure(4), plot(2:number_of_clusters,best_J_k_medians_zscore(2:end))
 title('K-medians elbow plot - zscore normalization')
 hold off
 
-[best_thetas_k_medians_min_max,best_bel_k_medians_min_max,best_J_k_medians_min_max] = k_algorithms(min_max_normalization',10, 1000, 'k_medians');
+[best_thetas_k_medians_min_max,best_bel_k_medians_min_max,best_J_k_medians_min_max] = k_algorithms(min_max_normalization',number_of_clusters, 1000, 'k_medians');
 % plot elbow curve to find the number of clusters
-figure(5), plot(2:10,best_J_k_medians_min_max(2:end))
+figure(5), plot(2:number_of_clusters,best_J_k_medians_min_max(2:end))
 title('K-medians elbow plot - min max normalization')
 hold off
 
 % k-medoids
-[best_thetas_k_medoids_zscore,best_bel_k_medoids_zscore,best_J_k_medoids_zscore] = k_algorithms(standard_score',10, 10, 'k_medoids');
+[best_thetas_k_medoids_zscore,best_bel_k_medoids_zscore,best_J_k_medoids_zscore] = k_algorithms(standard_score',number_of_clusters, 10, 'k_medoids');
 % plot elbow curve to find the number of clusters
-figure(6), plot(2:10,best_J_k_medoids_zscore(2:end))
+figure(6), plot(2:number_of_clusters,best_J_k_medoids_zscore(2:end))
 title('K-medoids elbow plot - zscore normalization')
 hold off
 
-[best_thetas_k_medoids_min_max,best_bel_k_medoids_min_max,best_J_k_medoids_min_max] = k_algorithms(min_max_normalization',10, 10, 'k_medoids');
+[best_thetas_k_medoids_min_max,best_bel_k_medoids_min_max,best_J_k_medoids_min_max] = k_algorithms(min_max_normalization',number_of_clusters, 10, 'k_medoids');
 % plot elbow curve to find the number of clusters
-figure(7), plot(2:10,best_J_k_medoids_min_max(2:end))
+figure(7), plot(2:number_of_clusters,best_J_k_medoids_min_max(2:end))
 title('K-medoids elbow plot - min max normalization')
 hold off
 
 % probalistic with gaussian mixture models
-[best_thetas_gmm_zscore,best_bel_gmm_zscore,best_J_gmm_zscore] = k_algorithms(standard_score',10, 1000, 'probalistic_gmm');
+[best_thetas_gmm_zscore,best_bel_gmm_zscore,best_J_gmm_zscore] = k_algorithms(standard_score',number_of_clusters, 1000, 'probalistic_gmm');
 % plot elbow curve to find the number of clusters
-figure(8), plot(2:10,best_J_gmm_zscore(2:end))
+figure(8), plot(2:number_of_clusters,best_J_gmm_zscore(2:end))
 title('Gausian Mixture models elbow plot - zscore normalization')
 hold off
 
-[best_thetas_gmm_min_max,best_bel_gmm_min_max,best_J_gmm_min_max] = k_algorithms(min_max_normalization',10, 1000, 'probalistic_gmm');
+[best_thetas_gmm_min_max,best_bel_gmm_min_max,best_J_gmm_min_max] = k_algorithms(min_max_normalization',number_of_clusters, 1000, 'probalistic_gmm');
 % plot elbow curve to find the number of clusters
-figure(9), plot(2:10,best_J_gmm_min_max(2:end))
+figure(9), plot(2:number_of_clusters,best_J_gmm_min_max(2:end))
 title('Gausian Mixture models elbow plot - min max normalization')
 hold off
 
@@ -95,6 +102,27 @@ hold off
 cluster_1 = country(find(best_bel_k_means_zscore{3} == 1))
 cluster_2 = country(find(best_bel_k_means_zscore{3} == 2))
 cluster_3 = country(find(best_bel_k_means_zscore{3} == 3))
+
+% Quantization
+% k-means
+
+k_means_zscore = Countrydata(find(best_bel_k_means_zscore{3} == 3),:);
+min_features_values = min(k_means_zscore);
+max_features_values = max(k_means_zscore);
+figure(1), hold on
+[~,length] = size(features_names);
+for i=1:length
+    subplot(3,3,i)
+    histogram(k_means_zscore(:,i));
+    title(features_names{i}) 
+end
+hold off
+
+%mean, standard devation per feature
+mean_features_values = mean(k_means_zscore);
+std_features_values = std(k_means_zscore);
+%matrix with correlation between features. Drop some highly corelated feautues(?)
+
 
 % To do: 
 % Check why for 1 cluster we have a smaller value than with other values . An indication that we
