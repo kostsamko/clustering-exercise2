b13d2d6d709ce73407d4343a9d4706e3efdeed68
TSAMKOSOGLOU Konstantinos <P70556@nrb.be>
2021-12-30 15:53:57 +0200

    quantization impl

---
+++ quantization.m
@@ -0,0 +1,39 @@
+function [clustering]=quantization(data,labels,clusters, bel, features_names, title_name)
+
+std_features_values = cell(1,clusters);
+clustering = cell(1,clusters);
+[figure_index,~] = size(get(0,'Children'));
+for i=1:clusters
+     cluster = data(find(bel{clusters} == i),:);
+     std_features_values{i} = std(cluster);
+     clustering{i} = labels(bel{clusters} == i);
+     figure(figure_index+i), hold on
+     [~,length] = size(features_names);
+     for j=1:length
+        subplot(3,3,j)
+        histogram(cluster(:,j));
+        title(features_names{j})
+        sgtitle(strcat(title_name,'quantization histogram',' cluster: ',int2str(i))) 
+     end
+end
+hold off
+[figure_index,~] = size(get(0,'Children'));
+
+min_values = min(data);
+max_values = max(data);
+
+figure(figure_index+1), hold on
+X = categorical(features_names);
+X = reordercats(X,features_names);
+legend_cell = cell(1,clusters);
+for i=1:clusters
+     subplot(3,4,i)
+     h = bar(X, diag(std_features_values{i}),'stacked');
+     title(strcat('cluster: ',int2str(i)));
+     sgtitle(strcat(title_name,' std bar plot'));
+     legend_cell{i} = strcat('min: ',sprintf('%.2f',min_values(i)),' max: ', sprintf('%.2f',max_values(i)));
+end
+set(h, {'DisplayName'}, legend_cell')
+lg = legend();
+lg.Location = 'northeastoutside';
+hold off
